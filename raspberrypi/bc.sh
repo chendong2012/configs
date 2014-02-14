4f92e1f75d042f5625827fafa940aae36c49f54b
chen dong <china.gd.sz.cd@gmail.com>
2014-02-14 17:14:41 +0800

    update

---
+++ raspberrypi/bc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+/opt/hub/configs/raspberrypi/bc_get_info.sh 0755 /opt/hub/configs/raspberrypi/test
+/opt/hub/configs/raspberrypi/bc_get_info.sh 0753 /opt/hub/configs/raspberrypi/test
+/opt/hub/configs/raspberrypi/bc_get_info.sh 0751 /opt/hub/configs/raspberrypi/test
+/opt/hub/configs/raspberrypi/bc_exchange.sh /opt/hub/configs/raspberrypi/test
+
