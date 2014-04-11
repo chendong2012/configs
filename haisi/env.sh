bbae656e9f0aed1fc03b72cab355215ea80a983f
chen dong <china.gd.sz.cd@gmail.com>
2014-04-11 20:03:17 +0800

    update

---
+++ haisi/env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+PATH=$PATH:$PWD/out/toolchain/arm-hisiv100-linux/target/bin:$PWD/out/toolchain/arm-hisiv100nptl-linux/target/bin:$PWD/out/toolchain/nptl/arm-hisiv100-linux/target/bin
+export PATH
