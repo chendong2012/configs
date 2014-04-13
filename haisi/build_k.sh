f88bf949ea7ef84e8022e3dbd51fd8ef41b378f9
chen dong <china.gd.sz.cd@gmail.com>
2014-04-13 20:08:12 +0800

    update

--- haisi/build_k.sh
+++ haisi/build_k.sh
@@ -1,80 +1,102 @@
 #!/bin/bash
-help="Usage: $0 cfg {new|menuconfig|touch|cpcfg|new|remake|r|n}"
+. env.sh
+match=`which mkimage 2>&1 | grep -o "no mkimage"`
+if [ "$match" != "" ]; then
+	./build_pctools.sh
+fi
+help="Usage: $0 cfg{mini|full} {new|menuconfig|touch|cpcfg|new|remake|r|n}"
 
-compile="CROSS_COMPILE=arm-hisiv200-linux-"
+compile_100="CROSS_COMPILE=arm-hisiv100-linux-"
+compile_100_nptl="CROSS_COMPILE=arm-hisiv100nptl-linux-"
+compile_200="CROSS_COMPILE=arm-hisiv200-linux-"
 CHIP_KERNEL_CFG="hi3520d_full_defconfig"
-echo $CHIP_KERNEL_CFG $compile
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$#" == "0" ]; then
 	echo $help
 	exit 0
 fi
 
+#$1 100 or 100_nptl or 200
+#$2 mini or full
 
-if [ ! -d kernel/linux-3.0.y ]; then
-	tar xzvf kernel/linux-3.0.y.tgz -C kernel/
-fi
+function create
+{
+	pushd kernel
+	if [ ! -d "$1"-"$2" ]; then
+		tar xzvf linux-3.0.y.tgz 
+		mv linux-3.0.y "$1"-"$2"
+	fi
+	popd
 
-case "$1" in
-mini)
-	CHIP_KERNEL_CFG="hi3520d_mini_defconfig"
-	;;
-full)
-	CHIP_KERNEL_CFG="hi3520d_full_defconfig"
-	;;
-*)
-	echo $help
-	exit 1
-esac
+	case "$2" in
+	mini)
+		CHIP_KERNEL_CFG="hi3520d_mini_defconfig"
+		;;
+	full)
+		CHIP_KERNEL_CFG="hi3520d_full_defconfig"
+		;;
+	*)
+		echo $help
+		exit 1
+	esac
 
-pushd kernel/linux-3.0.y
+	pushd kernel/$1-$2
 
 
-case "$2" in
-  clean)
-        make ARCH=arm "$compile" clean
-	;;
-  r*)
-        make ARCH=arm "$compile" -j 20
-        make ARCH=arm "$compile" uImage -j 20
-	;; 	
-  n*)
-        make ARCH=arm "$compile" clean
-        make ARCH=arm "$compile" "$CHIP_KERNEL_CFG"
-        make ARCH=arm "$compile" -j 20
-        make ARCH=arm "$compile" uImage -j 20
-	;;
-  menuconfig)
-	if [ ! -f .config ]; then
-		echo "[no config file]: make menuconfig error!!!"
+	case "$3" in
+	  clean)
+		make ARCH=arm "$4" clean
+		;;
+	  r*)
+		make ARCH=arm "$4" -j 20
+		make ARCH=arm "$4" uImage -j 20
+		mkdir -p ../../out/kernel/$1-$2
+		cp arch/arm/boot/uImage ../../out/kernel/$1-$2/
+		;; 	
+	  n*)
+		make ARCH=arm "$4" clean
+		make ARCH=arm "$4" "$CHIP_KERNEL_CFG"
+		make ARCH=arm "$4" -j 20
+		make ARCH=arm "$4" uImage -j 20
+		mkdir -p ../../out/kernel/$1-$2
+		cp arch/arm/boot/uImage ../../out/kernel/$1-$2/
+		;;
+	  menuconfig)
+		if [ ! -f .config ]; then
+			echo "[no config file]: make menuconfig error!!!"
+			popd
+			exit 1
+		fi
+		make ARCH=arm "$4" menuconfig
+		;;
+	  touch)
+		find . | xargs touch
+		;;
+	  cpcfg)
+		if [ $2 == "mini" ]; then
+			echo "cp arch/arm/configs/hi3520d_mini_defconfig .config"
+			cp arch/arm/configs/hi3520d_mini_defconfig .config
+		fi
+
+		if [ $2 == "full" ]; then
+			echo "cp arch/arm/configs/hi3520d_full_defconfig .config"
+			cp arch/arm/configs/hi3520d_full_defconfig .config
+		fi
+		;;
+	unpack)
+		popd
+		rm -rf kernel/"$1"-"$2"
+		tar xzvf kernel/linux-3.0.y.tgz -C kernel/
+		mv linux-3.0.y "$1"-"$2"
+		exit 1
+		;;
+	  *)
+		echo $help
 		popd
 		exit 1
-	fi
-        make ARCH=arm "$compile" menuconfig
-	;;
-  touch)
-        find . | xargs touch
-	;;
-  cpcfg)
-  	if [ $1 == "mini" ]; then
-		echo "cp arch/arm/configs/hi3520d_mini_defconfig .config"
-		cp arch/arm/configs/hi3520d_mini_defconfig .config
-	fi
-
-  	if [ $1 == "full" ]; then
-		echo "cp arch/arm/configs/hi3520d_full_defconfig .config"
-		cp arch/arm/configs/hi3520d_full_defconfig .config
-	fi
-	;;
-unpack)
+	esac
 	popd
-	rm -rf kernel/linux-3.0.y
-	tar xzvf kernel/linux-3.0.y.tgz -C kernel/
-	exit 1
-	;;
-  *)
-	echo $help
-	popd
-	exit 1
-esac
-popd
+}
+create "100" $1 $2 "$compile_100"
+create "100_nptl" $1 $2 "$compile_100_nptl"
+create "200" $1 $2 "$compile_200"
--- haisi/build_u.sh
+++ haisi/build_u.sh
@@ -3,10 +3,12 @@
 MKBOOT="mkboot-hi3520d.sh"
 REG_INFO_BIN="reg_info_Hi3520D-bvt_No1_660_330_660_ddr_innerFEPHY.bin"
 REG_INFO_BIN1="reg_info_Hi3515A-bvt_No1_600_300_600_ddr_innerFEPHY.bin"
-CHIP_BOOT_CFG="hi3520d_config"
 UBOOT_IMAGE_BIN="u-boot_hi3520d.bin"
 CHIP_UBOOT_CFG="hi3520d_config"
-compile="CROSS_COMPILE=arm-hisiv200-linux-"
+
+compile_100="CROSS_COMPILE=arm-hisiv100-linux-"
+compile_100_nptl="CROSS_COMPILE=arm-hisiv100nptl-linux-"
+compile_200="CROSS_COMPILE=arm-hisiv200-linux-"
 
 help="Usage: $0 cfg {new|remake|r|n}"
 
@@ -15,35 +17,48 @@ if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$#" == "0" ]; then
 	exit 0
 fi
 
-if [ ! -d uboot/u-boot-2010.06 ]; then
-	pushd uboot
-	tar xzvf u-boot-2010.06.tgz
-	popd
-fi
+#$1 100 or 100-nptl or 200
+#$2 operate new or remake or other ..
+#$3  cross-compile
+function create
+{
+	if [ ! -d uboot/"$1" ]; then
+		pushd uboot
+		tar xzvf u-boot-2010.06.tgz
+		mv u-boot-2010.06 "$1"
+		popd
+	fi
 
-if [ ! -d out/uboot ]; then
-	mkdir -p out/uboot
-fi
+	mkdir -p out/uboot/$1
 
-cp tools/pc_tools/uboot_tools/$MKBOOT out/uboot/
-cp tools/pc_tools/uboot_tools/$REG_INFO_BIN out/uboot/
-cp tools/pc_tools/uboot_tools/$REG_INFO_BIN1 out/uboot/
+	cp tools/pc_tools/uboot_tools/$MKBOOT out/uboot/"$1"
+	cp tools/pc_tools/uboot_tools/$REG_INFO_BIN out/uboot/"$1"
+	cp tools/pc_tools/uboot_tools/$REG_INFO_BIN1 out/uboot/"$1"
 
-case "$1" in
-n*)
-	pushd uboot/u-boot-2010.06
-	echo $PWD
-	make ARCH=arm "$compile" "$CHIP_UBOOT_CFG"
-	make ARCH=arm "$compile" -j 20
-	cp u-boot.bin ../../out/uboot/
-	popd
-	pushd out/uboot
-	chmod a+x ./$MKBOOT
-	./$MKBOOT $REG_INFO_BIN $REG_INFO_BIN1 $UBOOT_IMAGE_BIN
-	popd
-	;;
-*)
-	echo $help
-	exit 1
-esac
-echo "ok!"
+	case "$2" in
+	n*)
+		pushd uboot/$1
+		echo $PWD
+		make ARCH=arm "$3" "$CHIP_UBOOT_CFG"
+		make ARCH=arm "$3" -j 20
+		cp u-boot.bin ../../out/uboot/$1
+		popd
+		pushd out/uboot/$1
+		chmod a+x ./$MKBOOT
+		./$MKBOOT $REG_INFO_BIN $REG_INFO_BIN1 $UBOOT_IMAGE_BIN
+		popd
+		;;
+	*)
+		echo $help
+		return
+	esac
+	echo "ok!"
+}
+
+
+create "100" "$1" "$compile_100"
+create "100-nptl" "$1" "$compile_100_nptl"
+create "200" "$1" "$compile_200"
+echo =====================end========================
+
+
