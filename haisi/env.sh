eaa1b5ceddfa9c0bdabfcfce0c4c100af5be3cbb
chen dong <china.gd.sz.cd@gmail.com>
2014-04-16 14:55:54 +0800

    update

--- haisi/env.sh
+++ haisi/env.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 PATH=$PATH:$PWD/out/toolchain/arm-hisiv100-linux/target/bin:$PWD/out/toolchain/nptl/arm-hisiv100-linux/target/bin:$PWD/out/toolchain/arm-hisiv200-linux/target/bin:$PWD/out/pc_tools/bin
 export PATH
+
+match=`rpm -q libstdc++-statics | grep -o "not installed"`
+if [ $match == "not installed" ]; then
+	exit 1
+#sudo yum install ld-linux.so.2
+#sudo yum install libz.so.1
+fi
 echo $PATH
+exit 0
